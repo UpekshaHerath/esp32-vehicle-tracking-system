10e72c85c6cf7690cf7adb37c4af1ba7f11f2e67
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-28 19:18:29 +0530

    fix: removed the api key

--- front_end/src/components/GoogleMaps.jsx
+++ front_end/src/components/GoogleMaps.jsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 const GoogleMapsLoader = () => {
   return (
     <Script
-      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+      src={`https://maps.googleapis.com/maps/api/js?key=&libraries=places`}
       strategy="beforeInteractive"
     />
   );
